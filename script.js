78e8dce35cfb1f2c0d4f465e030fdc7e6ab27935
M Roja Rani <88879816+rojarani26@users.noreply.github.com>
2023-05-21 01:03:56 +0530

    Objects in local storage

--- script.js
+++ script.js
@@ -33,5 +33,18 @@ function getValue(){
                localStorage.setItem("gender",gender[i].value);
             }
         }
+
+
+        //Storing objects
     
 }
+
+let myObj = {
+    Firstname : document.getElementById('firstName').value,
+    Lastname : document.getElementById('lastName').value,
+};
+
+let myObj_serialized = JSON.stringify(myObj);
+localStorage.setItem("myObj", myObj_serialized);
+let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+console.log(myObj_deserialized); 
